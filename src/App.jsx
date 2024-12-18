7f3305c9bdd995fd8f243cbb50ab94e6a9578fb2
Tanjir Hasan <130759310+tanjirhc@users.noreply.github.com>
2024-12-18 08:59:16 +0600

    nav element

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Header from './Header.jsx'
 function App() {
   
   return(
-    <Header></Header>
+    <Header/>
   );
 }
 
--- src/Header.jsx
+++ src/Header.jsx
@@ -3,6 +3,14 @@ function Header(){
   return(
     <Header>
       <h1>My Website</h1>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Services</li>
+          <li>Contact</li>
+        </ul>
+      </nav>
     </Header>
   );
 }
